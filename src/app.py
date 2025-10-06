94495d3cf9c8599dd6b7d248c35053c1209cc52a
Fabio Motezuki <fabio.kenji@gmail.com>
2025-10-06 16:25:06 +0000

    adiciona dependências necessárias ao requirements.txt e reformata  a função app.py e indexing.py

--- src/app.py
+++ src/app.py
@@ -1,24 +1,30 @@
+''' Este app faz a leitura de um arquivo PDF e permite um chat simples com informações
+extraídas do documento.'''
+
 import gradio as gr
 from indexing import process_file
 
 
-# Função para chat (simples, apenas ecoa a mensagem)
 def chat_fn(message, history):
-	history = history or []
-	response = f"Você disse: {message}"
-	history.append((message, response))
-	return "", history
+    '''Função de chat simples que ecoa a mensagem do usuário.'''
+    history = history or []
+    response = f"Você disse: {message}"
+    history.append((message, response))
+    return "", history
+
 
 with gr.Blocks() as demo:
-	gr.Markdown("# RAG with Langchain and Gradio")
-	with gr.Row():
-		with gr.Column():
-			file_input = gr.File(label="Envie um arquivo - RAG", file_types=['.pdf'])
-			file_output = gr.Textbox(label="Status do arquivo", lines=40)
-			file_btn = gr.Button("Processar arquivo")
-			file_btn.click(process_file, inputs=file_input, outputs=[file_output])
-		with gr.Column():
-			gr.ChatInterface(chat_fn)
+    gr.Markdown("# RAG with Langchain and Gradio")
+    with gr.Row():
+        with gr.Column():
+            file_input = gr.File(
+                label="Envie um arquivo - RAG", file_types=['.pdf'])
+            file_output = gr.Textbox(label="Status do arquivo", lines=40)
+            file_btn = gr.Button("Processar arquivo")
+            file_btn.click(process_file, inputs=file_input, # pylint: disable=no-member
+                           outputs=[file_output])
+        with gr.Column():
+            gr.ChatInterface(chat_fn)
 
 if __name__ == "__main__":
-	demo.launch()
+    demo.launch()
--- src/indexing.py
+++ src/indexing.py
@@ -1,17 +1,23 @@
-from langchain_community.document_loaders import PyMuPDFLoader
+'''Modulo para indexação de documentos e extração de informações.'''
+
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
-# Função para processar o arquivo enviado
-def process_file(file):
-	if file is None:
-		return "Nenhum arquivo enviado."
-	loader = PyMuPDFLoader(file)
-	document = loader.load()
-	text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-	texts = text_splitter.split_documents(document)
-	embeddings = HuggingFaceEmbeddings()
-	docsearch = Chroma.from_documents(texts, embeddings, persist_directory="./chroma_db")
 
-	return f"Arquivo recebido: {file.name}\n{texts}"
+def process_file(file):
+    '''Função para processar o arquivo enviado e extrair informações.'''
+
+    if file is None:
+        return "Nenhum arquivo enviado."
+
+    loader = PyMuPDFLoader(file)
+    document = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    embeddings = HuggingFaceEmbeddings()
+    docsearch = Chroma.from_documents(
+        texts, embeddings, persist_directory="./chroma_db")
+
+    return f"Arquivo recebido: {file.name}\n\n{texts}"
